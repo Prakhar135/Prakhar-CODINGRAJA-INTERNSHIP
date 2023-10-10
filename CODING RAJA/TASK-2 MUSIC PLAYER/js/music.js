78a80b28c8b90f6794fc90d36fbf1f9980f6ca14
PRAKHAR SONKER <113596547+Prakhar135@users.noreply.github.com>
2023-10-10 09:55:26 +0530

    Add files via upload

---
+++ CODING RAJA/TASK-2 MUSIC PLAYER/js/music.js
@@ -0,0 +1,17 @@
+export const musicList = [
+	{
+		artist: "Arijit Singh",
+		name: "Raabta",
+		src: "music-1",
+	},
+	{
+		artist: "Jubin Nautiyal",
+		name: "Chaleya",
+		src: "music-2",
+	},
+	{
+		artist: "Neha Kakkar",
+		name: "Sawan Aaya",
+		src: "music-3",
+	},
+];
---
+++ CODING RAJA/TASK-2 MUSIC PLAYER/music.js
@@ -0,0 +1,17 @@
+export const musicList = [
+	{
+		artist: "Sia",
+		name: "Chandelier",
+		src: "music-1",
+	},
+	{
+		artist: "Adele",
+		name: "Rolling in the deep",
+		src: "music-2",
+	},
+	{
+		artist: "Boney M",
+		name: "Rasputin",
+		src: "music-3",
+	},
+];
