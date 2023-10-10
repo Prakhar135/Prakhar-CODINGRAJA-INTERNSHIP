3d9f1455c3b75effcca2d409b9cb63af0e6de2f3
PRAKHAR SONKER <113596547+Prakhar135@users.noreply.github.com>
2023-10-10 09:58:02 +0530

    Update music.js

--- CODING RAJA/TASK-2 MUSIC PLAYER/music.js
+++ CODING RAJA/TASK-2 MUSIC PLAYER/music.js
@@ -1,17 +1,1 @@
-export const musicList = [
-	{
-		artist: "Sia",
-		name: "Chandelier",
-		src: "music-1",
-	},
-	{
-		artist: "Adele",
-		name: "Rolling in the deep",
-		src: "music-2",
-	},
-	{
-		artist: "Boney M",
-		name: "Rasputin",
-		src: "music-3",
-	},
-];
+	
